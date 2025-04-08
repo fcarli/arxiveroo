73992eae8a5e0c23164440426e90372fca84417b
Francesco Carli <francesco.carli94@gmail.com>
2025-04-08 16:39:01 +0100

    Update init to include the preference specific query

--- arxiveroo/tools/query/__init__.py
+++ arxiveroo/tools/query/__init__.py
@@ -1,5 +1,6 @@
+from .all_resources import fetch_all_papers
 from .arxiv import fetch_arxiv_papers
 from .bioarxiv import fetch_biorxiv_papers
 from .medrxiv import fetch_medrxiv_papers
 
-__all__ = ["fetch_arxiv_papers", "fetch_biorxiv_papers", "fetch_medrxiv_papers"]
+__all__ = ["fetch_all_papers", "fetch_arxiv_papers", "fetch_biorxiv_papers", "fetch_medrxiv_papers"]
